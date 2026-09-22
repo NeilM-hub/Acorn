6e7414feca9b81752c16610569034ebde9cdd73e
NeilM-hub <80101298+NeilM-hub@users.noreply.github.com>
2026-09-22 11:53:04 +0100

    feat: activate checklist coverage wording revision

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,6 +10,7 @@ final class Plugin
     {
         \Acorn\SafetyHealthcheck\Content\ConciseContentUpgrade::installIfNeeded();
         \Acorn\SafetyHealthcheck\Content\SimplifiedContentUpgrade::installIfNeeded();
+        \Acorn\SafetyHealthcheck\Content\ChecklistCoverageUpgrade::installIfNeeded();
         add_action('admin_enqueue_scripts', [$this, 'adminAssets']);
         add_action('admin_menu', [new \Acorn\SafetyHealthcheck\Admin\Menu(), 'register']);
         add_action('admin_post_acorn_hc_pdf', [new \Acorn\SafetyHealthcheck\Admin\AssessmentsPage(), 'downloadPdf']);
